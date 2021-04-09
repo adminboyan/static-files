1780e67b60999d22171cc32cb331837962bf4ae3
iboyan <2073379083@qq.com>
2021-04-09 13:16:25 +0800

    Create boyan.js

---
+++ dir/boyan.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+	msg();
+});
+
+//清空内容
+function clear_msg(){
+	$("#msg-content").empty();
+}
+function msg(){
+	//生成0-5的随机数
+	var num = parseInt(Math.random()*(4-0+1)+0,10);;
+	
+	var content = new Array();
+	content[0] = '伯言';
+	content[1] = '';
+
+	$("#msg-content").append(content[num]);
+	window.setTimeout("clear_msg()",9800);
+	window.setTimeout("msg()",10000);
+}
